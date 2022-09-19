1a2557cca974b383a3a903cce543f06dd380dee9
vchacin <arq.vchacin@gmail.com>
2022-09-19 20:19:52 -0300

    Ejercicios extras

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -8,8 +8,18 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
   // usa matchFunc para identificar elementos que matchien
 
+  // console.log(startEl.children);
   // TU CÓDIGO AQUÍ
+  if (matchFunc(startEl)){
+    resultSet.push(startEl);
+  } else {
+    for (let i = 0; i < startEl.children.length; i++){
+      let recorrido = traverseDomAndCollectElements(matchFunc, startEl.children[i]);
+      resultSet.push(...recorrido);
+    } 
+  }
   
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -22,7 +32,7 @@ var selectorTypeMatcher = function(selector) {
   // .
   // tag
   // tag.
-  console.log('selector: ', selector);
+  // console.log('selector: ', selector);
 
   if (selector[0] === '#') {
     return 'id';
@@ -32,11 +42,13 @@ var selectorTypeMatcher = function(selector) {
     return 'tag.class';
   } else if (!selector.includes('.') || !selector.includes('#')) {
     return 'tag';
+  } else if (selector.includes('>')) {
+    return 'child combinator';
+  } else if (selector.split(' ')[0] == typeof 'string' && selector.split(' ')[1] == typeof 'string' && !selector.includes('>')) {
+    return 'descendant combinator';
   } 
 
 };
-
-console.log(selectorTypeMatcher('div.intro'));
 
 // NOTA SOBRE LA FUNCIÓN MATCH
 // recuerda, la función matchFunction devuelta toma un elemento como un
@@ -45,15 +57,63 @@ console.log(selectorTypeMatcher('div.intro'));
 
 var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
+
+  // console.log('selectorType', selectorType);
+
   var matchFunction;
   if (selectorType === "id") { 
-   
+    matchFunction = (tipoDeSelector) => {
+      if (selector == '#' + tipoDeSelector.id){
+        return true;
+      }
+      return false;
+    }
   } else if (selectorType === "class") {
-    
+    matchFunction = (tipoDeSelector) => {
+      for (let i = 0; i< tipoDeSelector.classList.length; i++){
+        if (selector == '.' + tipoDeSelector.classList[i]){
+          return true;
+        }
+      }
+      return false;
+    }
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = (tipoDeSelector) => {
+      for (let i = 0; i < tipoDeSelector.classList.length; i++){
+        if (selector === tipoDeSelector.localName+"."+tipoDeSelector.classList[i]){
+          return true;
+        }
+      }
+      return false;
+    }
   } else if (selectorType === "tag") {
-    
+    matchFunction = (tipoDeSelector) => {
+      if (selector == tipoDeSelector.tagName.toLowerCase()){
+        return true;
+      }
+      return false;
+    }
+  } else if (selectorType === "child combinator") {
+
+    matchFunction = (tipoDeSelector) => {
+      console.log('child', tipoDeSelector)
+
+      if (selector == tipoDeSelector.tagName.toLowerCase()){
+        return true;
+      }
+      return false;
+    }
+
+  } else if (selectorType === "descendant combinator") {
+    console.log('descendant', tipoDeSelector)
+
+    matchFunction = (tipoDeSelector) => {
+      if (selector == tipoDeSelector.tagName.toLowerCase()){
+        return true;
+      }
+      return false;
+    }
+
   }
   return matchFunction;
 };
