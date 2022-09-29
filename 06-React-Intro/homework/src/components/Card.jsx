a3fcdfd17c2904a0e98a834c9b040e4dcf2ab556
vchacin <arq.vchacin@gmail.com>
2022-09-29 13:37:34 -0300

    Card lista

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -1,6 +1,32 @@
 import React from 'react';
 
-export default function Card(props) {
+/* 
+
+Esta tarjeta va a mostrar el nombre de una ciudad, con su temperatura máxima y mínima, y con una imagen que representa el estado del clima (soleado, nublado, etc..), además cuando el usuario haga click en la X de cerrar, se invocará una función que tambien viene como props.
+Este Componente va a recibir las siguientes props:
+
+
+max: Temperatura Máxima.
+min: Temperatura Mínima.
+name: Nombre de la ciudad.
+img: nombre de la imagen que se debe mostrar.
+onClose: recibe una función que se va a ejecutar cuando el usuario haga click en el botón de cerrar.
+
+*/
+
+export default function Card({max, min, name, img, onClose}) {
   // acá va tu código
-  return <div>Card Component</div>
+  return <div>
+            <div>
+              <button onClick={onClose}>X</button>
+            </div>
+            <div>
+              <h2>{name}</h2>
+                <p>Min {min}</p>
+                <p>Max {max}</p>
+            </div>
+            <div>
+              <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="svg" />
+            </div>
+        </div>
 };
