6809fb893373d12caf840692061d58a6658b20f9
vchacin <arq.vchacin@gmail.com>
2022-09-29 21:01:10 -0300

    Homework 06 lista

--- 06-React-Intro/homework/src/App.js
+++ 06-React-Intro/homework/src/App.js
@@ -9,6 +9,12 @@ function App() {
   return (
     <div className="App">
       <div>
+        <SearchBar
+          onSearch={(ciudad) => alert(ciudad)}
+        />
+      </div>
+      <hr />
+      <div>
         <Card
           max={Cairns.main.temp_max}
           min={Cairns.main.temp_min}
@@ -21,12 +27,6 @@ function App() {
       <div>
         <Cards
           cities={data}
-        />
-      </div>
-      <hr />
-      <div>
-        <SearchBar
-          onSearch={(ciudad) => alert(ciudad)}
         />
       </div>
     </div>
--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
+/*
+Este Componente recibe por props una función onSearch que recibe un parámetro (que será el nombre de la ciudad tomado desde el input pero de momento pueden pasarle uno cualquiera ya que aún no estamos utilizando estados dentro del componente). La función onSearch se debe ejecutar cuando se haga click en el botón Agregar.
+*/
+
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return <div>
+            <div>
+              <input type="text" placeholder='Ciudad...' />
+            </div>
+            <button onClick={() => props.onSearch('Buscando ciudad')}>Agregar</button>
+        </div>
 };
