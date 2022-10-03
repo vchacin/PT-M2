8df8056f58c77b40b3b5984d6847da5a5df5c849
vchacin <arq.vchacin@gmail.com>
2022-10-03 00:22:22 -0300

    Searchbar styles

--- 07-React-Estilos/homework/src/components/SearchBar.jsx
+++ 07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import styles from '../styles/SearchBar.module.css';
+
+/*
+Este Componente recibe por props una función onSearch que recibe un parámetro (que será el nombre de la ciudad tomado desde el input pero de momento pueden pasarle uno cualquiera ya que aún no estamos utilizando estados dentro del componente). La función onSearch se debe ejecutar cuando se haga click en el botón Agregar.
+*/
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return <div className={styles.container}>
+            <div>
+              <input className={styles.input} type="text" placeholder='Enter a city name' />
+            </div>
+            <button className={styles.highlight} onClick={() => props.onSearch('Buscando ciudad')}>Add</button>
+        </div>
 };
