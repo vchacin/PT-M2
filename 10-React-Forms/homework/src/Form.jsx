c16ccdf9f93afa8b89786a6c19dae1115226899d
vchacin <arq.vchacin@gmail.com>
2022-10-13 00:16:07 -0300

    Homework 09

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 export default function  Form() {
+
+  const [username, setUsername] = React.useState('')
+  const [password, setPassword] = React.useState('')
+
   return (
+    <form>
       <div>
-        Componente Form
+        <label>Username:</label>
+        <input type="text" name="username" onChange={(e) => setUsername(e.target.value)} value={username} />
       </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} value={password}/>
+      </div>
+      <input type="submit" value="Send" />
+    </form>
   )
 }
